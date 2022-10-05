808dbf6042dcab81fe845331d3f0999d54e67f02
IlikegettingMonkeyBrain <114947148+IlikegettingMonkeyBrain@users.noreply.github.com>
2022-10-05 14:02:39 -0400

    Add files via upload

---
+++ c# calculator.cs
@@ -0,0 +1,42 @@
+//Program to add two Numbers; single line comment
+using System;
+class Program {
+  public static void Main (string[] args) {
+    int num1;  //Declaration
+    int num2;  //Declaration
+    int answer;//Declaration
+    
+    int num3; //Declaration
+    int num4; //Declaration
+    int answer2;//Declaration
+    
+    int num5; //Declaration
+    int num6; //Declaration
+    int answer3; //Declaration
+    
+    int num7;//Declaration
+    int num8;//Declaration
+    int answer4;//Declaration
+
+    num1=40;
+    num2=20;
+    answer=num1+num2; //Intialization
+    
+    num3=20;
+    num4=50;
+    answer2=num3-num4; //Intialization
+
+    num5=40;
+    num6=50;
+    answer3=num5*num6; //Intialization
+
+    num7=40;
+    num8=20;
+    answer4=num7/num8; //Intialization
+    
+    Console.WriteLine ("answer="+answer);
+    Console.WriteLine ("answer="+answer2);
+    Console.WriteLine ("answer="+answer3);
+    Console.WriteLine ("answer="+answer4);
+  }
+}

41f825f6accfbdd8a848662e0fecfa24cd613741
IlikegettingMonkeyBrain <114947148+IlikegettingMonkeyBrain@users.noreply.github.com>
2022-11-01 14:13:37 -0400

    Create add first 10 numbers.cs

---
+++ add first 10 numbers.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("The First 10 Natural Number are:");
+int x = 1, sum = 0;
+for (x = 1; x <= 10; x++)
+{
+Console.Write(x + " ");
+sum = sum + x;
+}
+Console.WriteLine();
+Console.WriteLine("The Sum is : " + sum);

3eda756223d26a79b42982002a9c13dd18d0c957
IlikegettingMonkeyBrain <114947148+IlikegettingMonkeyBrain@users.noreply.github.com>
2022-11-03 13:45:26 -0400

    Create multiple choice questions.cs

---
+++ multiple choice questions.cs
@@ -0,0 +1,97 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome To The Programming Quiz");
+    Console.WriteLine ("a. What is a variable");
+    Console.WriteLine ("1. a number");
+    Console.WriteLine ("2. a letter");
+    Console.WriteLine ("3. coding language");
+    Console.WriteLine ("4. variable is a place holder where you keep your value it also represents your value");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt1 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt1 == 4)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+        Console.WriteLine ("b. What is a compiler");
+    Console.WriteLine ("1. coding language");
+    Console.WriteLine ("2. a compiler is a computer program that translates computer code written in one programming language (the source language) into another language (the target language).");
+    Console.WriteLine ("3. reads the code");
+    Console.WriteLine ("4. type of application");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt2 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt2 == 2)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+              Console.WriteLine ("c. What is a declaration");
+    Console.WriteLine ("1. a declaration determines the name and data type of a variable or other element.");
+    Console.WriteLine ("2. computer program that translates computer code written in one programming language (the source language) into another language (the target language).");
+    Console.WriteLine ("3. types code for you");
+    Console.WriteLine ("4. a robot");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt3 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt3 == 1)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+      Console.WriteLine ("d. What is a if and else");
+    Console.WriteLine ("1. a variable");
+    Console.WriteLine ("2. a a monkey");
+    Console.WriteLine ("3. decison structure");
+    Console.WriteLine ("4. ancient monkey language");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt4 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt4 == 4)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+      Console.WriteLine ("a. What is a interger");
+    Console.WriteLine ("1. a number");
+    Console.WriteLine ("2. a letter");
+    Console.WriteLine ("3. coding language");
+    Console.WriteLine ("4. variable is a place holder where you keep your value it also represents your value");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt5 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt5 == 1)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+      
+      
+
+
+    
+
+    
+  }
+}

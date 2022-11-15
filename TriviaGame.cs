8115ae1eee89ac049c69bc03c3b2bf35dcee0274
IlikegettingMonkeyBrain <114947148+IlikegettingMonkeyBrain@users.noreply.github.com>
2022-11-15 13:15:37 -0500

    Create TriviaGame.cs

---
+++ TriviaGame.cs
@@ -0,0 +1,97 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome To Our Trivia");
+    Console.WriteLine ("a. How many blue stripes are there on the U.S. flag?");
+    Console.WriteLine ("1. 6");
+    Console.WriteLine ("2. 7");
+    Console.WriteLine ("3. 13");
+    Console.WriteLine ("4. 0");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt1 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt1 == 3)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+        Console.WriteLine ("b. What is the name for the Jewish New Year?");
+    Console.WriteLine ("1. Hanukkah");
+    Console.WriteLine ("2. Yom Kippur");
+    Console.WriteLine ("3. Kwanza");
+    Console.WriteLine ("4. Rosh Hashanah");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt2 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt2 == 4)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+              Console.WriteLine ("c. Which one of these characters is not friends with Harry Potter?");
+    Console.WriteLine ("1. Ron Weasley");
+    Console.WriteLine ("2. Neville Longbottom");
+    Console.WriteLine ("3. Draco Malfoy");
+    Console.WriteLine ("4. Hermione Granger");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt3 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt3 == 3)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+      Console.WriteLine ("d. What is the color of Donald Duck’s bowtie?");
+    Console.WriteLine ("1. Red");
+    Console.WriteLine ("2. Yellow");
+    Console.WriteLine ("3. Blue");
+    Console.WriteLine ("4. White");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt4 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt4 == 1)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+      Console.WriteLine ("a. What was the name of the band Lionel Richie was a part of?");
+    Console.WriteLine ("1. King Harvest");
+    Console.WriteLine ("2. Spectrums");
+    Console.WriteLine ("3. Commodores");
+    Console.WriteLine ("4. The Marshall Tucker Band");
+    Console.WriteLine ("Type the number corresponding to the answer you pick");
+        int opt5 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt5 == 3)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+      
+      
+
+
+    
+
+    
+  }
+}
